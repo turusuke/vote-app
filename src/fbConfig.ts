110894fbc664e797c6b2049f2cba5336feebd4f4
turusuke <mail@turusuke.com>
2020-02-04 08:08:48 +0900

    feat: add env

---
+++ src/fbConfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`,
+  // databaseURL: `https://${process.env.REACT_APP_PROJECT_ID}.firebaseio.com`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  // storageBucket: `${process.env.REACT_APP_PROJECT_ID}.appspot.com`,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: `1:${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}:web:65165098f1a7b6779ac284`
+}
