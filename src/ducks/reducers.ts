0e6c0674e1e0e69a96267c6e8c008386e36f2d04
turusuke <mail@turusuke.com>
2020-02-11 13:25:02 +0900

    chore: add state types

--- src/ducks/reducers.ts
+++ src/ducks/reducers.ts
@@ -3,9 +3,36 @@ import { firebaseReducer } from "react-redux-firebase";
 import { StateType } from "typesafe-actions";
 import { firestoreReducer } from "redux-firestore";
 
+interface profileType {
+  firebase: {
+    profile: {
+      avatarUrl: string;
+      displayName: string;
+      email: string;
+      providerData: [{}];
+    };
+  };
+}
+
+export interface Themes {
+  id: string;
+  title: string;
+  like: string[];
+  comment: string;
+  createdtime: string;
+};
+
+export interface firestoreType {
+  firestore: {
+    themes: Themes
+  };
+}
+
 export const reducers = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer
 });
 
-export type RootState = StateType<typeof reducers>;
+export type RootState = StateType<typeof reducers> &
+  profileType &
+  firestoreType;
