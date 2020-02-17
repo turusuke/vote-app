9385134df628cc1ae3cd3fef04334828cd200f59
turusuke <mail@turusuke.com>
2020-02-18 00:56:29 +0900

    chore: add redux dev tools

--- src/ducks/store.ts
+++ src/ducks/store.ts
@@ -5,6 +5,10 @@ export default function configureStore(initialState = {}) {
   return createStore(
     reducers,
     initialState,
+    process.env.NODE_ENV !== "production"
+      ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      : undefined
     // applyMiddleware()
   );
 }
