acbbbf89c929c058964ed23f87b5e72f9e327a21
turusuke <mail@turusuke.com>
2020-02-18 02:09:47 +0900

    chore: nits

--- src/ducks/store.ts
+++ src/ducks/store.ts
@@ -4,11 +4,11 @@ import { reducers } from "./reducers";
 export default function configureStore(initialState = {}) {
   return createStore(
     reducers,
-    initialState,
-    process.env.NODE_ENV !== "production"
-      ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-      : undefined
+    initialState
+    // process.env.NODE_ENV !== "production"
+    //   ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    //       (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    //   : undefined
     // applyMiddleware()
   );
 }
